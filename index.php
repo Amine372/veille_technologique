0654fe39aa3caf54c6db5ea4fb4bcb2c098b4548
amine372 <nakhila@orange.fr>
2020-10-13 22:01:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -502,17 +502,17 @@
                 <div class="row d-flex justify-content-between align-items-center">
                     <div class="col-xl-9 col-lg-8">
                         <div class="footer-copy-right">
-                            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                            <p>
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> Tous droits Réservés | Ce site a été réalisé par Amine NAKHIL
+</p>
                         </div>
                     </div>
                     <div class="col-xl-3 col-lg-4">
                         <!-- Footer Social -->
                         <div class="footer-social f-right">
-                            <span>Follow Us</span>
+                            <span>Abonez vous !</span>
                             <a href="#"><i class="fab fa-twitter"></i></a>
-                            <a href="https://www.facebook.com/sai4ull"><i class="fab fa-facebook-f"></i></a>
+                            <a href="https://www.facebook.com/profile.php?id=100008510403643"><i class="fab fa-facebook-f"></i></a>
                             <a href="#"><i class="fas fa-globe"></i></a>
                             <a href="#"><i class="fab fa-instagram"></i></a>
                         </div>
