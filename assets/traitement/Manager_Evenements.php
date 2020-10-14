e91363679d32d8f027d7d75fdc30c64d40f3905c
amine372 <nakhila@orange.fr>
2020-10-14 18:23:17 +0200

    commencer le traitement php de la page contact

--- assets/traitement/Manager_Evenements.php
+++ assets/traitement/Manager_Evenements.php
@@ -3,11 +3,11 @@ require_once(__DIR__.'/../modele/Model_Evenements.php');
 class Manager_Evenements
 {
 
-  protected $_id;
-  protected $_id_utilisateur;
-  protected $_description;
-  protected $_date;
-  protected $_Comm;
+  protected $_message;
+  protected $_nom;
+  protected $_prenom;
+  protected $_mail;
+  protected $_envoyer;
 
   public function Evenements()
   {
