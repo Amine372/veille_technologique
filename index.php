879d10064fe8fd350df91e4d2ddf07330c73d9be
amine372 <nakhila@orange.fr>
2020-10-16 22:37:52 +0200

    e

--- index.php
+++ index.php
@@ -397,7 +397,7 @@
                 </div>
                 <div class="col-lg-3 col-md-4 col-sm-6">
                     <div class="single-brand text-center mb-30">
-                        <img src="assets/img/gallery/brand4.png" alt="">
+                        <img src="assets/img/logo/scripting.jpg" alt="">
                         <p>Batch, VBS</p>
                         <p>Bon niveau/p>
                     </div>
