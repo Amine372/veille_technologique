78038302b8194fdd9da33b4f4dbda02c258f964a
amine372 <nakhila@orange.fr>
2021-01-15 17:33:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -210,6 +210,7 @@
                 <div class="col-xl-5 col-lg-4 col-md-6 col-sm-6">
                     <div class="gallery-box">
                         <div class="single-gallery">
+                          <a href="https://example.com">
                             <div class="gallery-img" style="background-image: url(assets/img/gallery/gallery1.png);"></div>
                             <div class="cap-icon">
                                 <a href="assets/img/gallery/gallery1.png" class="ti-fullscreen img-pop-up"></a>
@@ -219,6 +220,7 @@
                                 <p>Duis aute irure dolor in reprehenderit in voluptate velit.</p>
                             </div>
                         </div>
+                        </a>
                     </div>
                 </div>
                 <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
