6793126af327516b8735a4f6286473fb0158320e
amine372 <nakhila@orange.fr>
2021-01-21 19:17:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -371,7 +371,7 @@
                 </div>
                 <div class="col-lg-3 col-md-4 col-sm-6">
                     <div class="single-brand text-center mb-30">
-                        <img src="assets/img/logo/C-language.png" alt="">
+                        <img src="assets/img/logo/C-language1.png" alt="">
                         <p>C</p>
                         <p>Exellent niveau</p>
                     </div>
