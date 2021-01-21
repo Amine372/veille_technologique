2e943364d5c25f1a0e93843bc76c26d62742bca3
amine372 <nakhila@orange.fr>
2021-01-21 17:31:28 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -97,7 +97,7 @@
             <div class="container">
                 <div class="d-none d-sm-block mb-5 pb-4">
                     <div id="map" style="height: 480px; position: relative; overflow: hidden;">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10490.40678275778!2d2.47242775!3d48.90392755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6132b990a1c0d%3A0x18560381f1a5059a!2s4%20All%C3%A9e%20Rouget%20de%20Lisle%2C%2093130%20Noisy-le-Sec!5e0!3m2!1sfr!2sfr!4v1611246217091!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10490.40678275778!2d2.47242775!3d48.90392755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6132b990a1c0d%3A0x18560381f1a5059a!2s4%20All%C3%A9e%20Rouget%20de%20Lisle%2C%2093130%20Noisy-le-Sec!5e0!3m2!1sfr!2sfr!4v1611246217091!5m2!1sfr!2sfr" width="1200" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 </div>
                 <div class="row">
                     <div class="col-10">
